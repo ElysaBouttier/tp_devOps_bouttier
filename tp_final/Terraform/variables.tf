b99b9cef0249050631d4270a676e1d5953827418
Ubuntu <ubuntu@ip-172-31-2-192.eu-west-3.compute.internal>
2021-01-25 17:57:10 +0000

    ok

---
+++ tp_final/Terraform/variables.tf
@@ -0,0 +1,21 @@
+variable "region" {
+  type = string
+  description = "AWS region"
+  default = "eu-west-3"
+}
+
+variable "instance_type" {
+  type = string
+  description = "instance type"
+  default = "t2.micro"
+}
+
+variable "tag_name" {
+  type = string
+  description = "tag name"
+  default = "instance_jenkins_server_bouttier"
+}
+
+
+
+
